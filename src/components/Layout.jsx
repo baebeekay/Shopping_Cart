bd5555ef0c6177db03a5d94dcc1e4dddaf83886f
baebeekay <baebeekay6@gmail.com>
2025-12-11 17:41:50 +0100

    Create Layout component with Navbar and Outlet

---
+++ src/components/Layout.jsx
@@ -0,0 +1,14 @@
+
+import Navbar from './Navbar';
+import { Outlet } from 'react-router-dom';
+
+export default function Layout() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <main className="max-w-7xl mx-auto px-4 py-8">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
